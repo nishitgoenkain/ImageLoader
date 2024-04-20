eb091414e99d3ef0384f81f79c223a8a61d7cab5
nishant <nishant@iassureit.in>
2024-04-20 15:35:27 +0530

    Initial Commit

---
+++ ImageLoader/ImageLoaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageLoaderApp.swift
+//  ImageLoader
+//
+//  Created by Nishit Goenka on 20/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ImageLoaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
